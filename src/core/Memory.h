8b1e177aab89711540784ac0f42f676d9c37fd38
Noyau <noyau.dev@gmail.com>
2023-05-19 17:24:12 +0200

    core: exposed WeakPtr

--- src/core/Memory.h
+++ src/core/Memory.h
@@ -4,6 +4,7 @@
 
 template<typename T> using UniquePtr = std::unique_ptr<T>;
 template<typename T> using SharedPtr = std::shared_ptr<T>;
+template<typename T> using WeakPtr = std::weak_ptr<T>;
 
 template<typename T, typename... Args>
 constexpr UniquePtr<T> MakeUnique(Args&&... args)
